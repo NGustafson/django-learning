97ccca6eadfa2c9783ac54ddfdb34eba3464c3c6
Nicholas Gustafson <ngustafson247@gmail.com>
2024-09-14 14:30:37 +0900

    show similar posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -58,6 +59,14 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tag_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     return render(
         request,
         'blog/post/detail.html',
@@ -65,6 +74,7 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'form': form,
+            'similar_posts': similar_posts,
         }
     )
 
