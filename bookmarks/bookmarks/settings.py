0e79d3e32f9fd170db667b268b7aa35cb87f5f4a
Nicholas Gustafson <ngustafson247@gmail.com>
2024-10-14 14:30:13 +0900

    Add image ranking and view counts with redis. Finish chapter 7.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -46,11 +46,13 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'easy_thumbnails',
+    'debug_toolbar',
     'images.apps.ImagesConfig',
     'actions.apps.ActionsConfig',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -164,3 +166,11 @@ SOCIAL_AUTH_PIPELINE = [
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username]),
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
