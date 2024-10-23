5be6fac8ff01fe5d2b214ac828d2476975c0ef18
Nicholas Gustafson <ngustafson247@gmail.com>
2024-10-23 21:44:29 +0900

    Add link to stripe payment

--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -40,6 +40,8 @@ def stripe_webhook(request):
 
             # Mark order as paid
             order.paid = True
+            # Store Stripe payment ID
+            order.stripe_id = session.payment_intent
             order.save()
 
     return HttpResponse(status=200)
