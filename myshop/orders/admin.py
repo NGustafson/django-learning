66daba4c1e33d220f392d438c3dd509837ae2b7c
Nicholas Gustafson <ngustafson247@gmail.com>
2024-10-28 15:52:09 +0900

    Add admin enhancements for orders and payments.  Finish chapter 9.

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,4 +1,9 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
@@ -20,8 +25,55 @@ def order_payment(obj):
 order_payment.short_description = 'Stripe payment'
 
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = (
+        f'attachment; filename={opts.verbose_name}.csv'
+    )
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%Y/%m/%d')
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+
+order_pdf.short_description = 'Invoice'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    actions = [export_to_csv]
+
     list_display = [
         'id',
         'first_name',
@@ -34,6 +86,8 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         'created',
         'updated',
+        order_detail,
+        order_pdf,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from orders.models import Order
-
+from .tasks import payment_completed
 
 @csrf_exempt
 def stripe_webhook(request):
@@ -43,5 +43,8 @@ def stripe_webhook(request):
             # Store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+
+            # launch asynchronous task
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
