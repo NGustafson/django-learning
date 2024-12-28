9facd5e448e7d1061d14454fe53b2e2d8961256b
Nicholas Gustafson <ngustafson247@gmail.com>
2024-12-28 16:01:55 +0900

    Add students to courses and buttons for course enrollment/registration

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -10,6 +10,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from django.views.generic.list import ListView
 
+from students.forms import CourseEnrollForm
 from .forms import ModuleFormSet
 from .models import Course, Module, Content, Subject
 
@@ -212,6 +213,14 @@ class CourseListView(TemplateResponseMixin, View):
             }
         )
 
+
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+            initial={'course': self.object}
+        )
+        return context
