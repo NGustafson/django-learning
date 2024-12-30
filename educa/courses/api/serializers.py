ef4d4562666496e17b3350b42baebff5798a528a
Nicholas Gustafson <ngustafson247@gmail.com>
2024-12-30 16:18:12 +0900

    Add endpoint to get course contents for authenticated and enrolled users

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from rest_framework import serializers
 
-from courses.models import Course, Subject, Module
+from courses.models import Content, Course, Subject, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -39,6 +39,44 @@ class ModuleSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created',
+            'owner',
+            'modules',
+        ]
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
 
     class Meta:
         model = Course
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -8,7 +8,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseSerializer, CourseWithContentsSerializer, SubjectSerializer
 from courses.models import Course, Subject
 
 
@@ -44,3 +45,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(
+        detail=True,
+        methods=['get'],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
