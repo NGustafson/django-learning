28869b76362267146fe85510f97603611e4519b6
Nicholas Gustafson <ngustafson247@gmail.com>
2024-12-30 15:17:39 +0900

    Add course enroll endpoint to API and require authentication

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,5 +1,10 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
@@ -27,3 +32,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
