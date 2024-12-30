db6654c6e0cfb5a14eaa31765795ee0be5cbd11a
Nicholas Gustafson <ngustafson247@gmail.com>
2024-12-30 15:32:17 +0900

    Move enroll endpoint to Course ViewSet

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -33,12 +34,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
 
-
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
