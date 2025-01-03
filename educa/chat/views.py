9cb2118aaa6c25e0e543e6b1522854a0932d38d8
Nicholas Gustafson <ngustafson247@gmail.com>
2025-01-03 21:04:11 +0900

    Add chat history when loading chat room.  Finish chapter 16.

--- educa/chat/views.py
+++ educa/chat/views.py
@@ -14,8 +14,17 @@ def course_chat_room(request, course_id):
         # User is not a student of the course, or the course does not exist
         return HttpResponseForbidden()
 
+    # Retrieve chat history
+    latest_messages = course.chat_messages.select_related(
+        'user'
+    ).order_by('-id')[:5]
+    latest_messages = reversed(latest_messages)
+
     return render(
         request,
         'chat/room.html',
-        {'course': course}
+        {
+            'course': course,
+            'latest_messages': latest_messages
+        }
     )
