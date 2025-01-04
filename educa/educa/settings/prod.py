57537145f6546118682854835042ce421e2bebf5
Nicholas Gustafson <ngustafson247@gmail.com>
2025-01-04 22:09:25 +0900

    Setup production environment using docker compose.

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -1,13 +1,25 @@
+from decouple import config
 from .base import *
 
 DEBUG = False
+
 ADMINS = [
     ('Nick G', 'admin@localhost'),
 ]
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
